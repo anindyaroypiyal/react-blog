bb659981b67837c9f34c4f0df845a6a5100b0d74
Anindya Roy Piyal <anindya.roy.piyal@g.bracu.ac.bd>
2023-03-20 00:04:34 +0600

    added write post

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -7,7 +7,7 @@ Sidebar() {
         <div className="sidebarItem">
             <span className="sidebarTitle">ABOUT ME</span>
             <img
-          src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
+          src="https://myfirstwebsites.neocities.org/abdalla/abme.jpg"
           alt=""
         />
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse, doloremque aperiam. Magnam deleniti provident reprehenderit perspiciatis sapiente exercitationem distinctio incidunt praesentium modi harum.</p>
---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,25 @@
+import "./singlePost.css"
+
+export default function SinglePost() {
+  return (
+    <div className="singlePost">
+        <div className="singlePostWrapper">
+            <img src="https://images.pexels.com/photos/2821728/pexels-photo-2821728.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" className="singlePostImg" />
+            <h1 className="singlePostTitle">
+                Lorem ipsum dolor sit amet consectetur.
+                <div className="singlePostEdit">
+            <i className="singlePostIcon far fa-edit"></i>
+            <i className="singlePostIcon far fa-trash-alt"></i>
+          </div>
+            </h1>
+            <div className="singlePostInfo">
+                <span className="singlePostAuthor">Author: <b>Roy</b></span>
+                <span className="singlePostDate">1 hour ago</span>
+            </div>
+            <p className="singlePostDesc">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea corrupti iste itaque vitae labore nemo fuga animi aperiam totam alias illum quisquam tenetur laudantium et molestiae saepe, officiis mollitia repellendus. Blanditiis minima deleniti velit accusamus quis excepturi fuga, facilis architecto eligendi, esse corrupti ipsa adipisci ipsam tenetur, similique quo vitae harum! Tempore ipsam cupiditate placeat odio facere similique non natus illum officia excepturi fugit, ratione nesciunt atque et fuga eius harum impedit magnam minus magni dolor ducimus? Perferendis, illum? Aspernatur velit sed officiis libero eos doloremque aliquam labore facilis, nemo id exercitationem qui, enim, totam beatae ut quidem inventore vero temporibus et assumenda dolor reiciendis consequatur consectetur? Ratione corporis, ex similique nulla id, asperiores harum, expedita non ducimus nam omnis? Voluptatum ex tempora voluptatem culpa! Quae totam ex voluptates molestiae impedit consectetur ipsam expedita, culpa alias minima laudantium ea illum aut mollitia iste delectus. Et, modi, debitis iste, quaerat adipisci necessitatibus reiciendis voluptatem quasi quidem minus sint vitae eum saepe suscipit? Fugit consectetur odio totam ducimus sequi nemo, repudiandae voluptas ullam eaque blanditiis, aperiam laudantium vitae sint quae quaerat esse quod ipsa voluptatibus autem quia? Illo facere reiciendis mollitia consequuntur non, natus, cupiditate adipisci, deserunt sint quos ea. Tenetur nihil repellendus itaque architecto beatae soluta mollitia maxime dolorum porro? Aspernatur fuga officia reiciendis beatae illo labore similique id quidem, omnis atque sequi dolorem nesciunt et voluptas sit. Tenetur alias reprehenderit commodi dolore dolorem voluptates perspiciatis, ex facilis architecto corrupti eius, iusto aliquam at, necessitatibus maiores labore sequi consequuntur cupiditate voluptate impedit voluptatum molestiae inventore fuga. Accusantium ad accusamus sit autem ab maxime. Consectetur aut perspiciatis voluptas quaerat aperiam facere vel, autem similique dolorum consequatur magni laudantium quisquam. Excepturi ea voluptas fugit quo saepe debitis cum, accusamus tempora architecto sed odio voluptatibus quas doloribus ex incidunt ullam mollitia repellendus? Quos, consequuntur.
+            </p>
+        </div>
+    </div>
+  )
+}
