5fcd760be9e4c5bfddbfaf6bb6972b22b15c7b88
Anindya Roy Piyal <anindya.roy.piyal@g.bracu.ac.bd>
2023-04-01 20:14:43 +0600

    blog app working project

--- client/src/pages/settings/Settings.jsx
+++ client/src/pages/settings/Settings.jsx
@@ -1,40 +1,100 @@
-import "./settings.css"
-import Sidebar from "../../components/sidebar/Sidebar"
+import "./settings.css";
+import Sidebar from "../../components/sidebar/Sidebar";
+import { useContext, useState } from "react";
+import { Context } from "../../context/Context";
+import axios from "axios";
 
 export default function Settings() {
+  const [file, setFile] = useState(null);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const { user, dispatch } = useContext(Context);
+  const PF = "http://localhost:5000/images/"
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "UPDATE_START" });
+    const updatedUser = {
+      userId: user._id,
+      username,
+      email,
+      password,
+    };
+    if (file) {
+      const data = new FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      updatedUser.profilePic = filename;
+      try {
+        await axios.post("/upload", data);
+      } catch (err) {}
+    }
+    try {
+      const res = await axios.put("/users/" + user._id, updatedUser);
+      setSuccess(true);
+      dispatch({ type: "UPDATE_SUCCESS", payload: res.data });
+    } catch (err) {
+      dispatch({ type: "UPDATE_FAILURE" });
+    }
+  };
   return (
     <div className="settings">
-        <div className="settingsWrapper">
-          <div className="settingsTitle">
-            <span className="settingsUpdateTitle">Update Your Account</span>
-            <span className="settingsDeleteTitle">Delete Account</span>
-          </div>
-          <form className="settingsForm">
-            <label>Profile Picture</label>
-            <div className="settingsPP">
-              <img src="https://images.pexels.com/photos/15784891/pexels-photo-15784891.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
-              <label htmlFor="fileInput">
-              <i className="settingsPPIcon far fa-user-circle"></i>{" "}
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update Your Account</span>
+          <span className="settingsDeleteTitle">Delete Account</span>
+        </div>
+        <form className="settingsForm" onSubmit={handleSubmit}>
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src={file ? URL.createObjectURL(file) : PF+user.profilePic}
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon far fa-user-circle"></i>
             </label>
             <input
-              id="fileInput"
               type="file"
+              id="fileInput"
               style={{ display: "none" }}
-              className="settingsPPInput"
+              onChange={(e) => setFile(e.target.files[0])}
             />
-            </div>
-            <label>Username</label>
-          <input type="text" placeholder="Roy" name="name" />
+          </div>
+          <label>Username</label>
+          <input
+            type="text"
+            placeholder={user.username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
           <label>Email</label>
-          <input type="email" placeholder="roy28@gmail.com" name="email" />
+          <input
+            type="email"
+            placeholder={user.email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <label>Password</label>
-          <input type="password" placeholder="Password" name="password" />
-          <button className="settingsSubmitButton" type="submit">
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="settingsSubmit" type="submit">
             Update
           </button>
-          </form>
-        </div>  
-        <Sidebar/>
+          {success && (
+            <span
+              style={{ color: "green", textAlign: "center", marginTop: "20px" }}
+            >
+              Profile has been updated...
+            </span>
+          )}
+        </form>
+      </div>
+      <Sidebar />
     </div>
-  )
+  );
 }
